9d2fbb381da73b6eca4a1393e87fd2c434f11482
abledenthusiast <Pitmana@xavier.edu>
2018-03-05 17:43:48 -0500

    Many fixes.

--- trie.go
+++ trie.go
@@ -1,3 +1,6 @@
+// You can edit this code!
+// Click here and start typing.
+package main
 /* Aaron Pitman */
 
 import (
@@ -9,7 +12,6 @@ type trie struct {
 }
 
 type node struct {
-	key string
 	isWord bool
 	next [26]*node
 }
@@ -18,44 +20,49 @@ func New() *trie {
 	return &trie{nil}
 }
 
-func (trieNode trie) contains(word string) bool {
-	if trieNode is nil {
+func (trieNode *trie) contains(word string) bool {
+	if trieNode == nil {
 		return false
 	}
 	current := trieNode.root
 	for i := 0; i < len(word); i++ {
-		if current.next[word[i]] is nil {
+		if current.next[getIndex(word[i])] == nil {
 			return false
-		}
-		else {
-			current = current.next[word[i]]
+		} else {
+			current = current.next[getIndex(word[i])]
 		}
 	}
 	return current.isWord
 }
 
-func (trieNode trie) insert(word string) *node {
-	if trieNode is nil {
+func (trieNode *trie) insert(word string) {
+	if trieNode.root == nil {
 		trieNode.root = newNode()
 	}
 	current := trieNode.root
 	for i := 0; i < len(word); i++ {
-		if current.next[word[i]] != nil {
-			current = current.next
-		}
-		else {
-			current.next[word[i]] = newNode()
-			current = current.next[word[i]]
+		if current.next[getIndex(word[i])] != nil {
+			current = current.next[getIndex(word[i])]
+		} else {
+			current.next[getIndex(word[i])] = newNode()
+			current = current.next[getIndex(word[i])]
 		}
 	}
 	current.isWord = true
 }
 
 func newNode() *node {
-	var next [256]*node
-	return &node{nil,nil,next}
+	var next [26]*node
+	return &node{false,next}
+}
+
+func getIndex(charVal byte) int {
+	return int(charVal - 'a')
 }
 
 func main() {
 	fmt.Println("Hey, this is WIP. Stop judging.")
+	trie := New()
+	trie.insert("hello")
+	fmt.Println(trie.contains("hello"))
 }
